09711efa9f2fc638f6e1ce78c4a7954baa27dac1
Cristian <cabrusilt@utn.edu.ec>
2021-06-02 15:50:08 -0500

    segundo

--- cnn.js
+++ cnn.js
@@ -1,10 +1,10 @@
 const pgPromise = require ('pg-promise')
 const config={
-    host:'localhost',
+    host:'ec2-34-230-115-172.compute-1.amazonaws.com',
     port:'5432',
-    database:'pizza-app',
-    user:'postgres',
-    password:'cristian'
+    database:'dco2ghhel0sn8',
+    user:'nvidbtwusdiwoo',
+    password:'f8f05be8fa9cd03d41c0955bd7e1559c61b5cf090dbfd1f113455502ca012115'
 }
 const pgp=pgPromise({})
 const db=pgp(config)
