9bbffa1a0159c9daf6cdbd484213ea39a0ec51be
Cristian <cabrusilt@utn.edu.ec>
2021-06-03 10:15:28 -0500

    ter

--- cnn.js
+++ cnn.js
@@ -1,7 +1,7 @@
 const pgPromise = require ('pg-promise')
 const config={
     host:'ec2-34-230-115-172.compute-1.amazonaws.com',
-    port:'5432',
+    port:'8000',
     database:'dco2ghhel0sn8',
     user:'nvidbtwusdiwoo',
     password:'f8f05be8fa9cd03d41c0955bd7e1559c61b5cf090dbfd1f113455502ca012115'
